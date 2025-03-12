a52b105fa206755c90df7be3bb01ab3baf6a2a7b
revaza1andria <matejaniashvili12@gmail.com>
2025-03-12 17:43:24 +0400

    commit

---
+++ day 04/homework/homework.py
@@ -0,0 +1,19 @@
+x = 100
+y = 130
+z = 90
+
+age = 12
+age1 = 4
+print(age + age1)
+
+name = "andria"
+surname = "revazashvili"
+print(name + " " + surname)
+
+name = "mariam"
+print(name)
+name = "andria"
+print(name)
+
+number = "5"
+print("result " + number)
