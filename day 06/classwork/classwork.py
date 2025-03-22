4fab070ba9375fc8c223bfafedd5a4a95fcbb2cd
revaza1andria <matejaniashvili12@gmail.com>
2025-03-22 15:08:51 +0400

    commit

---
+++ day 06/classwork/classwork.py
@@ -0,0 +1,17 @@
+#classwork N1
+age = input ('pls write your age ')
+print(age)
+#classwork N2
+name = input ("what is your name ")
+print('hello ' + name)
+#classwork N3
+number = input ('please input first number: ')
+number1 = input ('please input second number: ')
+print(int(number) + int(number1))
+print(int(number) - int(number1))
+print(int(number) * int(number1))
+print(int(number) / int(number1))
+#classwork N4
+name = input('please input your name: ')
+age = input('please input your age: ')
+print(name +  ' you are ' + age + ' years old')
---
+++ day 06/homework/homework.py
@@ -0,0 +1,9 @@
+num1 = input('please input any number: ')
+print(num1)
+age = input('input your age: ')
+print(type(age))
+num2 = '15'
+print(int(num2))
+
+#input-->საშუალებას გვაძლევს ტერმინალიდან შევიყვანოთ გარკვეული ინფორმაცია
+#type -->ფუნქცია ამოწმებს მონაცემის ტიპს(data typeს)
