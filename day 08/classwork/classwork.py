fdda00c71b09124c6ebb62505eaeabdef68a51a0
revaza1andria <matejaniashvili12@gmail.com>
2025-04-09 16:28:35 +0400

    commit

---
+++ day 08/classwork/classwork.py
@@ -0,0 +1,29 @@
+#classwork N1
+
+num = input("please input any number here: ")
+num1 = input ('and here: ')
+
+print(num < num1)
+print(num1 > num)
+print(num1 == num)
+
+print(127 == 127)
+print(156 < 451)
+print(516 > 342)
+print(342 > 234)
+print(456 < 678)
+
+print(234 <123)
+print(312 == 232)
+print(434 > 532)
+print(3453 < 2)
+print(2141 > 2623) 
+
+#classwork N2
+
+myheight = int(input('please input your height: '))
+friend_height = int(input('input your friend height: '))
+
+print('you are taller than toyr friend: ', myheight > friend_height)
+print('your friend taller than you: ', friend_height > myheight)
+print('you and your friend are the same height: ', myheight == friend_height)
---
+++ day 08/homework/homework.py
@@ -0,0 +1,17 @@
+num = 281
+num1 = 162
+
+print(num > num1)
+print(num < num1)
+print(num == num1)
+print(num >= num1)
+print(num <= num1)
+
+#შედარებები: მეტობა -- >, ნაკლებობა <, ტოლობა ==
+
+num2 = 10
+num3 = input ('please intup any number: ')
+
+#
+
+print (int(num2) <= int(num3))
